7328df14d056f9bd70ed2b44b7e5d974de2eb572
Alex Jaldin <ajaldin@frba.utn.edu.ar>
2026-05-02 15:09:35 -0300

    main.cpp

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    string Nombre, Apellido;
+    
+    cout<<"Ingrese su nombre";cin>>Nombre;
+    cout<<"Ingrese su apellido";cin>>Apellido;
+
+    cout<<"Hola "<<Nombre<<" "<<Apellido<<endl;
+
+    return 0;
+}
